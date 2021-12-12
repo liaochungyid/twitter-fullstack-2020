50bd0d2e34269c1231a7dfdc12473438af44aade
Luo, Shi-Wei <yulaie1012@gmail.com>
2021-12-12 14:03:27 +0800

    feat: update PrivateMessage model

--- seeders/20211211142952-privateMessages-seed-file.js
+++ seeders/20211211142952-privateMessages-seed-file.js
@@ -8,8 +8,8 @@ module.exports = {
         senderId: 11,
         receiverId: 21,
         text: 'User1 to User2',
-        unread: true,
-        createdAt: new Date(),
+        unread: false,
+        createdAt: new Date(new Date() - 86400 + 1000 * 1),
         updatedAt: new Date()
       },
       {
@@ -17,8 +17,8 @@ module.exports = {
         senderId: 21,
         receiverId: 11,
         text: 'User2 to User1',
-        unread: true,
-        createdAt: new Date(),
+        unread: false,
+        createdAt: new Date(new Date() - 86400 + 1000 * 2),
         updatedAt: new Date()
       },
       {
@@ -26,8 +26,8 @@ module.exports = {
         senderId: 11,
         receiverId: 21,
         text: 'User1 to User2',
-        unread: true,
-        createdAt: new Date(),
+        unread: false,
+        createdAt: new Date(new Date() - 86400 + 1000 * 3),
         updatedAt: new Date()
       },
       {
@@ -35,8 +35,62 @@ module.exports = {
         senderId: 11,
         receiverId: 31,
         text: 'User1 to User3',
-        unread: true,
-        createdAt: new Date(),
+        unread: false,
+        createdAt: new Date(new Date() - 86400 + 1000 * 4),
+        updatedAt: new Date()
+      },
+      {
+        id: 51,
+        senderId: 31,
+        receiverId: 11,
+        text: 'User3 to User1',
+        unread: false,
+        createdAt: new Date(new Date() - 86400 + 1000 * 5),
+        updatedAt: new Date()
+      },
+      {
+        id: 61,
+        senderId: 11,
+        receiverId: 31,
+        text: 'User1 to User3',
+        unread: false,
+        createdAt: new Date(new Date() - 86400 + 1000 * 6),
+        updatedAt: new Date()
+      },
+      {
+        id: 71,
+        senderId: 11,
+        receiverId: 21,
+        text: 'User1 to User2',
+        unread: false,
+        createdAt: new Date(new Date() - 86400 + 1000 * 7),
+        updatedAt: new Date()
+      },
+      {
+        id: 81,
+        senderId: 11,
+        receiverId: 21,
+        text: 'User1 to User2',
+        unread: false,
+        createdAt: new Date(new Date() - 86400 + 1000 * 8),
+        updatedAt: new Date()
+      },
+      {
+        id: 91,
+        senderId: 21,
+        receiverId: 11,
+        text: 'User2 to User1',
+        unread: false,
+        createdAt: new Date(new Date() - 86400 + 1000 * 9),
+        updatedAt: new Date()
+      },
+      {
+        id: 101,
+        senderId: 21,
+        receiverId: 11,
+        text: 'User2 to User1',
+        unread: false,
+        createdAt: new Date(new Date() - 86400 + 1000 * 10),
         updatedAt: new Date()
       }
     ])
